4d90e5851b0763b4398c4388297e1b84f8d9ea19
Piwko Patryk <patryk.piwko@god.dev>
2024-05-21 19:37:40 +0200

    Initial commit

---
+++ src/main/java/org/shelter/adpitionservice/AdpitionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.shelter.adpitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpitionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdpitionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shelter/adpitionservice/AdpitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shelter.adpitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
