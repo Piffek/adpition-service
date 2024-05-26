ed82636e3667c546561d22054d80ceaf7978078a
Piwko Patryk <patryk.piwko@god.dev>
2024-05-26 20:47:27 +0200

    discovery server

--- src/main/java/org/shelter/adoptionservice/AdoptionServiceApplication.java
+++ src/main/java/org/shelter/adoptionservice/AdoptionServiceApplication.java
@@ -3,6 +3,7 @@ package org.shelter.adoptionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "org.shelter.adoptionservice.infra")
 @EntityScan(basePackages = "org.shelter.adoptionservice.infra.entity")
 @ComponentScan(basePackages = "org.shelter")
+@EnableDiscoveryClient
 public class AdoptionServiceApplication {
 
     public static void main(String[] args) {
